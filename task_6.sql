096e935434a167f9f28611cdb502737ddf8242b4
Yhutee Elijah <Utibeelijah11@gmail.com>
2025-02-09 22:45:16 +0100

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 -- Insert multiple customers into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
