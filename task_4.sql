c8bb840b2ca98eca5c429476df84dbf55a991723
Yhutee Elijah <Utibeelijah11@gmail.com>
2025-02-09 22:26:17 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,6 @@
+-- Select the correct database
+USE alx_book_store;
+
 -- Get full details of the books table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
