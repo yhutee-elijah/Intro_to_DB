e4885b4270c5261c88419f28fa50383ae4da71e4
Yhutee Elijah <Utibeelijah11@gmail.com>
2025-02-09 22:34:34 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,6 @@
+-- Select the correct database
+USE alx_book_store;
+
 -- Insert a new customer into the Customers table
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
