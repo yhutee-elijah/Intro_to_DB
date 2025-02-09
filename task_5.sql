e3b78d3739dc7519dcd3347e797f1442c9e7bbaa
Yhutee Elijah <Utibeelijah11@gmail.com>
2025-02-09 22:40:08 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -2,5 +2,5 @@
 USE alx_book_store;
 
 -- Insert a new customer into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
